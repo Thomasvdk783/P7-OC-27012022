b4f2e91fc98011c713d788c84a9eb4e8a186db92
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-01-27 11:26:03 +0100

    initialisation du btn Ingrédients

--- main.js
+++ main.js
@@ -11,12 +11,64 @@ async function dataRecipes() {
   recipes.forEach((item) => {
     const arrIngredients = item.ingredients;
     let showIngredients;
+
+    const arrUstensiles = item.ustensils;
+    // console.log(arrUstensiles);
+    const allUstensiles = ustensils.concat(arrUstensiles);
+    console.log(allUstensiles);
+
     arrIngredients.forEach(function (ing) {
       const ingredient = ing.ingredient;
+      const arrIngredients = [ing.ingredient];
+      const allIngredientInArray = arrIngredients.concat();
+      // console.log(allIngredientInArray);
       const quantity = ing.quantity;
       const unit = ing.unit;
       showIngredients += `<li>${ingredient} : ${quantity} ${unit}</li>`;
+
+
+      /// BTN Ingredients ///
+      const btnAfficheIngredients = document.querySelector(".dropbtn1");
+      const dropContentIngredients = document.querySelector(".dropdown-content");
+      const showIngredientList = `<ul class="ul-btn-ingredients"><li>${ingredient}</li></ul>`
+      dropContentIngredients.innerHTML += showIngredientList;
+      
+      btnAfficheIngredients.addEventListener("click", function () {
+        dropContentIngredients.style.display = "block";
+        dropDownIngredient.classList.toggle("dropdown-ingredients-width");
+      });
+
+
+
+      /// BTN Ustensiles ///
+      const btnAfficheUstensiles = document.querySelector(".dropbtn3");
+      const dropContent3 = document.querySelector(".dropdown-content-ustensiles");
+      const dropDownUstensiles = document.querySelector(".dropdown-ustensiles");
+      // const showIngredientList = `<ul class="ul-btn-ingredients"><li>${ingredient}</li></ul>`
+      // dropContentIngredients.innerHTML += showIngredientList;
+    
+      btnAfficheUstensiles.addEventListener("click", function () {
+        dropContent3.style.display = "block";
+        dropDownUstensiles.classList.toggle("dropdown-ingredients-width");
+      });
     });
+
+    /// Appareils ///
+
+
+    /// BTN Appareil ///
+    const btnAfficheAppareils = document.querySelector(".dropbtn2");
+    const dropContent2 = document.querySelector(".dropdown-content-appareil");
+    const dropDownAppareils = document.querySelector(".dropdown-appareil");
+    // const showAppareilList = `<ul class="ul-btn-ingredients"><li>${appareils}</li></ul>`
+    // dropContentIngredients.innerHTML += showAppareilList;
+      
+    btnAfficheAppareils.addEventListener("click", function () {
+      dropContent2.style.display = "block";
+      dropDownAppareils.classList.toggle("dropdown-ingredients-width");
+    });
+
+    
 
     const containerRecipes = document.getElementById("list-recipes");
     const recipeCard = `<section class="card-recipe">
@@ -32,7 +84,7 @@ async function dataRecipes() {
                   </p>
               </header>
               <article class="ingredients-explication">
-              <ul class="show-ingredient">${showIngredients}</ul>
+              <ul class="show-ingredient">${showIngredients}
                   <p>
                       ${item.description}
                   </p>
@@ -41,76 +93,7 @@ async function dataRecipes() {
           </section>`;
 
     containerRecipes.innerHTML += recipeCard;
+
   });
 }
 dataRecipes();
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////
-/////////////////////////
-//// BTN appareil /////
-// import dataRecipes from "./dataApi.js";
-
-// export default function myFunctionAppareils() {
-//   const btnAfficheIngredients = document.querySelector(".dropbtn2");
-//   const dropContent = document.querySelector(".dropdown-content-appareil");
-//   const dropDownIngredient = document.querySelector(".dropdown-appareil");
-
-//   btnAfficheIngredients.addEventListener("click", function () {
-//     dropContent.style.display = "block";
-//     // dropDownIngredient.classList.toggle("dropdown-ingredients-width");
-//   });
-//   dataRecipes();
-// }
-// myFunctionAppareils();
-
-
-
-
-
-/////////////////////////////////////////////
-
-//// BTN appareil /////
-// import dataRecipes from "./dataApi.js";
-
-// export default function myFunctionAppareils() {
-//   const btnAfficheIngredients = document.querySelector(".dropbtn2");
-//   const dropContent = document.querySelector(".dropdown-content-appareil");
-//   const dropDownIngredient = document.querySelector(".dropdown-appareil");
-
-//   btnAfficheIngredients.addEventListener("click", function () {
-//     dropContent.style.display = "block";
-//     // dropDownIngredient.classList.toggle("dropdown-ingredients-width");
-//   });
-//   dataRecipes();
-// }
-// myFunctionAppareils();
-
-
-/////////////////////////////////////////////////////
-//// BTN ustensiles /////
-
-// import dataRecipes from "./dataApi.js";
-
-// export default function myFunctionUstensiles() {
-//   const btnAfficheIngredients = document.querySelector(".dropbtn3");
-//   const dropContent = document.querySelector(".dropdown-content-ustensiles");
-//   const dropDownIngredient = document.querySelector(".dropdown-ustensiles");
-
-//   btnAfficheIngredients.addEventListener("click", function () {
-//     dropContent.style.display = "block";
-//     // dropDownIngredient.classList.toggle("dropdown-ingredients-width");
-//   });
-//   dataRecipes();
-// }
-// myFunctionUstensiles();
-
-
