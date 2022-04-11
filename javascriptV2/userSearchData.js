59ba3933431de40671143ac563ec249fe00f2345
Thomasvdk783 <thomas.vandekerchove@outlook.com>
2022-04-11 19:20:43 +0200

    algo2

--- javascriptV2/userSearchData.js
+++ javascriptV2/userSearchData.js
@@ -1,42 +1,43 @@
-// Cette page va être appeler quand l'utilisateur va écrire dans la barre de recherche.
 import { fetchDataRecipe } from "./fetchDataRecipe.js";
 import { showRecipes } from "./showRecipes.js";
 
 export const userSearchData = async () => {
-  const searchBar = document.getElementById("searchbar");
+    const searchBar = document.getElementById("searchbar");
+    // Récupération de la Data
+    const data = await fetchDataRecipe();
+    const result = data.recipes; 
+    const resultSearch = searchBar.value.toLowerCase();
+    let showGoodCard = [];
 
-  // Tu récupères le Json
-  const data = await fetchDataRecipe();
-  const result = data.recipes;
-  let showGoodCard = [];
-  const resultSearch = searchBar.value.toLowerCase();
-
-  // Tu vérifies le nombre de caractère que l'utilisateur à entrer
-  if (resultSearch.length >= 3) {
-    result.forEach((item) => {
-      if (
-        item.name.toLowerCase().includes(resultSearch) === true ||
-        item.description.toLowerCase().includes(resultSearch) === true ||
-        item.appliance.toLowerCase().includes(resultSearch) === true
-      ) {
-        showGoodCard.push(item); // 5
-      } else {
-        item.ustensils.find((result) => {
-          if (result.toLowerCase().includes(resultSearch) === true) {
-            showGoodCard.push(item); // 5
-          }
-        });
-        const ingredients = item.ingredients;
-        ingredients.forEach((ing) => {
-          if (ing.ingredient.toLowerCase().includes(resultSearch) === true) {
-            showGoodCard.push(item); //
-          }
-        });
-      }
-    });
-  } else {
-    // Si l'utilisateur à écrit moins de 3 caractére, tu fais rien
-    showGoodCard = result;
-  }
-  showRecipes(showGoodCard);
+    if (resultSearch.length >= 3) {
+        for ( const item of result ) {
+            // console.log(item);
+            if (
+                item.name.toLowerCase().includes(resultSearch) === true ||
+                item.description.toLowerCase().includes(resultSearch) === true ||
+                item.appliance.toLowerCase().includes(resultSearch) === true
+            ){
+                showGoodCard.push(item)
+            }else {
+                item.ustensils.find((result) => {
+                    if (result.toLowerCase().includes(resultSearch) === true) {
+                      showGoodCard.push(item); // 5
+                    }
+                  }); 
+                  const ingredients = item.ingredients;
+                  for (let i = 0; i < ingredients.length; i++) {
+                    //   console.log(ingredients[i])
+                    const nameIng = ingredients[i].ingredient; 
+                    // console.log(nameIng)
+                        if(nameIng.toLowerCase().includes(resultSearch) === true){
+                            showGoodCard.push(item)
+                        }
+                  }
+            }
+        }
+    }
+    else{
+        showGoodCard = result;
+    }
+    showRecipes(showGoodCard);
 };
